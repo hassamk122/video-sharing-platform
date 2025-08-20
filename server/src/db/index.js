b0bb411f79da3108a9ea3fac2775eb0d2dc64dce
hassamk122 <hassamkiani66@gmail.com>
2025-08-20 18:37:05 +0500

    mongodb setup

---
+++ server/src/db/index.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+
+
+async function connectDatabase() {
+    try{
+        const connectionInstance = await mongoose.connect(`${process.env.LOCAL_DATABASE_URI}/${DB_NAME}`);
+        console.log(`\n Mongodb connected! DB host: ${connectionInstance.connection.host}`);
+    }catch(error){
+        console.log("Couldn't connect to monogodb",error);
+        process.exit(1);
+    }
+}
+
+export default connectDatabase;
---
+++ server/src/server.js
@@ -0,0 +1,8 @@
+import dotenv from "dotenv";
+import connectDatabase from "./db/index.js";
+
+dotenv.config({
+    path : "./env"
+});
+
+connectDatabase();
